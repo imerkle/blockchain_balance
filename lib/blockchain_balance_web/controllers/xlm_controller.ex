a3d45f7f15fa27658c37f7cfa3406ec215c53fa7
slim <dsslimshaddy@gmail.com>
2019-02-25 20:29:08 +0530

    xlm post

--- lib/blockchain_balance_web/controllers/xlm_controller.ex
+++ lib/blockchain_balance_web/controllers/xlm_controller.ex
@@ -2,7 +2,7 @@ defmodule BlockchainBalanceWeb.XlmController do
     use BlockchainBalanceWeb, :controller
 
     def post_tx(conn, %{"tx" => tx, "ticker"=> ticker}) do
-        api = @coins[ticker][api]
+        api = @coins[ticker]["api"]
         BlockchainBalance.Blockchain.post(api, %{"tx"=> tx})
         json(conn, %{})
     end
